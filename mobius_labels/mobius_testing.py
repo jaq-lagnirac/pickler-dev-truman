cc6dd2feaa7bd5a247e5f51366499da428d1e6a0
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-09-16 11:41:42 -0500

    Successfully implemented test form filler
    Uses fillpdf, not PyPDF2
    Now need to shrink to fit 8-to-a-page

--- mobius_labels/mobius_testing.py
+++ mobius_labels/mobius_testing.py
@@ -1,24 +1,33 @@
-from PyPDF2 import PdfReader, PdfWriter
+# from PyPDF2 import PdfReader, PdfWriter
+from fillpdf import fillpdfs
+# https://www.youtube.com/watch?v=TcBX2kb6g3o
+# https://pypi.org/project/fillpdf/
+# https://fillpdf.readthedocs.io/en/latest/
 import sys
 
 input_pdf = 'fillable_form.pdf'
-reader = PdfReader(input_pdf)
-page = reader.pages[0]
-fields = reader.get_form_text_fields()
+# reader = PdfReader(input_pdf)
+# page = reader.pages[0]
+# fields = reader.get_form_text_fields()
+# print(fields)
+
+output_pdf = 'output_form.pdf'
+# writer = PdfWriter()
+# writer.add_page(page)
+# print(writer.pages[0].extract_text())
+
+# writer.update_page_form_field_values(
+#     writer.pages[0], fields
+# )
+
+# with open(output_pdf, 'wb') as outfile:
+#     writer.write(outfile)
+
+fields = fillpdfs.get_form_fields(input_pdf)
 print(fields)
 
 for field in fields:
     fields[field] = 'this is a test'
 print(fields)
 
-output_pdf = 'output.pdf'
-writer = PdfWriter()
-writer.add_page(page)
-print(writer.pages[0].extract_text())
-
-writer.update_page_form_field_values(
-    writer.pages[0], fields
-)
-
-with open(output_pdf, 'wb') as outfile:
-    writer.write(outfile)
+fillpdfs.write_fillable_pdf(input_pdf, output_pdf, fields)
