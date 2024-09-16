f98fbd3f9f3fe183aa965a7451b514361d5eeb7f
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-09-16 11:50:02 -0500

    Condensed form filler example code

--- mobius_labels/mobius_testing.py
+++ mobius_labels/mobius_testing.py
@@ -1,4 +1,3 @@
-# from PyPDF2 import PdfReader, PdfWriter
 from fillpdf import fillpdfs
 # https://www.youtube.com/watch?v=TcBX2kb6g3o
 # https://pypi.org/project/fillpdf/
@@ -6,22 +5,7 @@ from fillpdf import fillpdfs
 import sys
 
 input_pdf = 'fillable_form.pdf'
-# reader = PdfReader(input_pdf)
-# page = reader.pages[0]
-# fields = reader.get_form_text_fields()
-# print(fields)
-
 output_pdf = 'output_form.pdf'
-# writer = PdfWriter()
-# writer.add_page(page)
-# print(writer.pages[0].extract_text())
-
-# writer.update_page_form_field_values(
-#     writer.pages[0], fields
-# )
-
-# with open(output_pdf, 'wb') as outfile:
-#     writer.write(outfile)
 
 fields = fillpdfs.get_form_fields(input_pdf)
 print(fields)
