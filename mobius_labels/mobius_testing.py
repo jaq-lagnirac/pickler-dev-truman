3e53fcbbfa13ead34e0c49a2563ba95b4cd8b3f8
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-09-16 12:09:00 -0500

    Implemented last try of PyPDF2 testing (still doesn't work)

--- mobius_labels/mobius_testing.py
+++ mobius_labels/mobius_testing.py
@@ -7,11 +7,35 @@ import sys
 input_pdf = 'fillable_form.pdf'
 output_pdf = 'output_form.pdf'
 
-fields = fillpdfs.get_form_fields(input_pdf)
-print(fields)
+def test_fillpdf():
+    fields = fillpdfs.get_form_fields(input_pdf)
+    print(fields)
 
-for field in fields:
-    fields[field] = 'this is a test'
-print(fields)
+    for field in fields:
+        fields[field] = 'this is a test'
+    print(fields)
 
-fillpdfs.write_fillable_pdf(input_pdf, output_pdf, fields)
+    fillpdfs.write_fillable_pdf(input_pdf, output_pdf, fields)
+
+from PyPDF2 import PdfReader, PdfWriter
+
+def test_pypdf2():
+    reader = PdfReader(input_pdf)
+    page = reader.pages[0]
+    fields = reader.get_form_text_fields()
+
+    for field in fields:
+        fields[field] = 'this is a test'
+    print(fields)
+
+    writer = PdfWriter()
+    writer.add_page(page)
+    print(writer.pages[0].extract_text())
+    writer.update_page_form_field_values(
+        writer.pages[0], fields
+    )
+
+    writer.write(f'pypdf_{output_pdf}')
+
+test_fillpdf()
+#test_pypdf2()
