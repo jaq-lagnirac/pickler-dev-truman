9962bbbb49be935091409b01fd83342ba4c0900b
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-09-18 12:13:12 -0500

    Testing out new Mobius Label

--- mobius_labels/mobius_testing.py
+++ mobius_labels/mobius_testing.py
@@ -13,7 +13,7 @@ def test_fillpdf():
     print(fields)
 
     for field in fields:
-        fields[field] = 'this is a test'
+        fields[field] = 'this is\na test'
     print(fields)
 
     fillpdfs.write_fillable_pdf(input_pdf, output_pdf, fields)
@@ -45,12 +45,39 @@ def test_pdf_to_img():
     # https://stackoverflow.com/a/70095504
     images = convert_from_path(output_pdf, poppler_path='Release-24.07.0-0\\poppler-24.07.0\\Library\\bin')
     for index, img in enumerate(images):
-        img.save(f'test{index}.png', 'PNG')
+        img.save(f'test{output_pdf}{index}.png', 'PNG')
 
     # doc = pymupdf.open('output_form.pdf')
     # for page in doc:
     #     pix = page.get_pixmap()
     #     pix.save(f'test{page.number}.png')
+
+
+mobius_pdf = 'mobius_label.pdf'
+mobius__output_pdf = 'output_form_mobius_newline.pdf'
+def test_new_mobius_label():
+    fields = fillpdfs.get_form_fields(mobius_pdf)
+    print(fields)
+    # {
+    #     'Location': '',
+    #     'CallNumber': '',
+    #     'Title': '',
+    #     'SendTo': '',
+    #     'Patron': '',
+    #     'SuppliedBy': '',
+    #     'DamageNotedOn': 'Off',
+    #     'DamagedNotedUponReturn': 'Off'
+    # }
+
+    for field in fields:
+        if field == 'DamageNotedOn' or field == 'DamagedNotedUponReturn':
+            fields[field] = 'Yes'
+            continue
+        fields[field] = 'this is a test\nmultiline1\nmultiline2\nmultiline3\nmultiline4'
+    print(fields)
+
+    fillpdfs.write_fillable_pdf(mobius_pdf, mobius__output_pdf, fields)
+    
 
 
 from reportlab.lib.pagesizes import letter
@@ -62,4 +89,5 @@ def test_reportlab_tables():
 
 # test_fillpdf()
 # test_pypdf2()
-test_pdf_to_img()
+# test_pdf_to_img()
+test_new_mobius_label()
