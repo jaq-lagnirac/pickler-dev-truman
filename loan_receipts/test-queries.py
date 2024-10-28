9b43fcb883d3af5a58b25f94455e5fbddacf1b88
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-10-28 12:14:33 -0500

    Added test formatting to receipt processing

--- loan_receipts/test-queries.py
+++ loan_receipts/test-queries.py
@@ -1,6 +1,6 @@
 # Justin Caringal, Stephen Wynn
 # 
-# [PROGRAM PURPOSE]
+# Testing grounds for querying FOLIO API and formatting text
 # 
 # Project start date: 2024-10-21
 # Project end date: YYYY-MM-DD
@@ -93,38 +93,53 @@ def login_folioclient() -> folioclient.FolioClient:
 
 f = login_folioclient()
 
-from datetime import datetime, timezone, timedelta
-# local_time_now = datetime.now().isoformat()
-now = datetime.now(timezone.utc)
-delta = timedelta(days=1, minutes=15)
-start_time = now - delta
-timeframe_start = start_time.isoformat()
-timeframe_end = now.isoformat()
-# print(local_time_now)
-print(timeframe_start)
-print(timeframe_end)
-print(timeframe_start < timeframe_end)
+test_full_card_id = '123456789'
+test_extracted_id = test_full_card_id[:-2]
+# print(f'{test_full_card_id}\n{test_extracted_id}')
 
-test_query1 = f'loanDate > \"{timeframe_start}\" and loanDate < \"{timeframe_end}\" and action == \"checkedout\"'
+from datetime import datetime, timezone, timedelta
+now = datetime.now(timezone.utc)
+delta = timedelta(days=7, minutes=15)
+cutoff_time = now - delta
+timeframe_start = cutoff_time.isoformat()
+# print(timeframe_start)
+
+barcode_query = '000010030'
+
+test_query1 = f'loanDate > \"{timeframe_start}\" and action == \"checkedout\"'
+test_query2 = 'item.status.name == \"Checked out\"'
 queries = f.folio_get_all(path=f'/circulation/loans',
                           key='loans',
                           query=test_query1)
 
-barcode_query = '000010030'
-
+# print('\n\n\n')
 from pprint import pprint
 checked_out_items = []
+total_queries = 0
+matched_queries = 0
+top_loan_date = None
 for query in queries:
     # pprint(query)
+    # continue
+    # print(query['item']['title'])
     barcode_borrower = query['borrower']['barcode']
+    total_queries += 1
     if barcode_borrower == barcode_query:
-
+        matched_queries += 1
         # pprint(query['borrower'])
 
         # extracts due date, coverts to a more human-readable format
         iso_due_date = datetime.fromisoformat(query['dueDate'])
         printable_due_date = iso_due_date.strftime('%a %d %b %Y, %I:%M%p')
+        # https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
         # print(f'{query['dueDate']}\t{iso_due_date}\t{printable_due_date}')
+
+        # extracts loan date, coverts to a more human-readable format
+        iso_loan_date = datetime.fromisoformat(query['loanDate'])
+        printable_loan_date = iso_loan_date.strftime('%a %d %b %Y, %I:%M%p')
+        # finds most recent loan date time
+        if not top_loan_date or printable_loan_date > top_loan_date:
+            top_loan_date = printable_loan_date
 
         item = query['item']
         item_dict = {
@@ -135,4 +150,49 @@ for query in queries:
         }
         checked_out_items.append(item_dict)
 
-pprint(checked_out_items)
+# pprint(checked_out_items)
+
+RECEIPT_TEXT_WIDTH = 50
+num_items = len(checked_out_items)
+plural_s = lambda : 's' if num_items != 1 else ''
+RECEIPT_HEADER = f'''
+Truman State University
+Pickler Memorial Library
+
+{top_loan_date}
+{num_items} item{plural_s()} checked out.
+
+'''
+def center_multiline_text(text, width):
+    lines = text.splitlines()
+    centered_lines = [line.center(width) for line in lines]
+    return '\n'.join(centered_lines)
+
+def print_items(items, width):
+    item_text = ''
+    for index, item in enumerate(items):
+        item_text += f'''
+ITEM {index + 1}
+TITLE: {item['title'][ : width - 7]}
+CALL #: {item['callNumber']}
+BARCODE: {item['barcode']}
+DUE DATE: {item['dueDate']}
+''' # 'TITLE: ' is 7 characters
+    return item_text
+    
+receipt_text = center_multiline_text(RECEIPT_HEADER, RECEIPT_TEXT_WIDTH) + \
+    print_items(checked_out_items, RECEIPT_TEXT_WIDTH)
+
+# TEST_RECEIPT = 'Truman State University'.center(text_width) + \
+#     '\n' + \
+#     'Pickler Memorial Libary'.center(text_width) + \
+#     '\n\n' + \
+#     top_loan_date.center(text_width) + \
+#     '\n' + \
+#     f'{num_items} item{plural_s()} checked out.'.center(text_width) + \
+#     '\n\n'
+
+print(receipt_text)
+
+with open('test-receipt.txt', 'w') as file:
+    file.write(receipt_text)
