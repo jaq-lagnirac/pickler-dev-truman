873a1761c328607ed524772a76e71374203a20c9
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-12-06 11:26:00 -0600

    Started new project, added test boilerplate
    Working on automating and streamlining the monthly budget report

---
+++ budget_report/test-budget.py
@@ -0,0 +1,61 @@
+# Justin Caringal
+# 
+# Testing grounds, working on autmating and
+# streamlining the monthly budget report
+# 
+# Project start date: 2024-12-06
+# Project end date: YYYY-MM-DD
+
+import folioclient
+
+# keys required in config.json
+REQUIRED_CONFIG_KEYS = {
+    'okapi_url' : 'https://okapi-mobius.folio.ebsco.com',
+    'tenant' : '[INSTITUTION ID]',
+    'username' : '[USERNAME]',
+    'password' : '[PASSWORD]',
+    }
+
+
+def login_folioclient() -> folioclient.FolioClient:
+    """Organizes initial handshake with FOLIOClient.
+    
+    A function which handles the possible exceptions on start-up
+    and, if everything is in order, logs into the FOLIOClient API.
+    
+    Args:
+        None
+    
+    Returns:
+        FolioClient: Returns an API object to the FOLIOClient
+    """
+
+    config_name = 'config.json'
+
+    # Setup FOLIO variables
+    login = None # scope resolution
+    with open(config_name ,'r') as config:
+        login = json.load(config)
+
+    # checks to ensure config file is set up correctly
+    required_key_names = set(REQUIRED_CONFIG_KEYS.keys())
+    # if required keys not in login
+    if not required_key_names.issubset(set(login.keys())):
+        print('bad keys smh')
+        return
+
+    # unpacks relevant data from config.json file
+    okapi_url = login['okapi_url']
+    tenant = login['tenant']
+    username = login['username']
+    password = login['password']
+
+    # attempts FOLIO API handshake
+    f = None # scope resolution
+    try:
+        f = folioclient.FolioClient(okapi_url, tenant, username, password)
+    except Exception as e:
+        print('can\'t connect, womp womp')
+        return
+
+    return f
