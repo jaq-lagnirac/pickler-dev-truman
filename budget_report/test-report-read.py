ea7b1781c475cccb023932b3be46cbc4dd7d42ae
Justin Caringal <99774975+jaq-lagnirac@users.noreply.github.com>
2024-12-06 12:20:03 -0600

    Added numerical columns indexing test

--- budget_report/test-report-read.py
+++ budget_report/test-report-read.py
@@ -3,3 +3,5 @@ import pandas as pd
 df = pd.read_csv('invoice-export-2024-12-05-08_32.csv')
 new_df = df['Invoice line fund distributions'].str.split('\"\"', expand=True)
 print(new_df)
+
+print(df.columns[0])
